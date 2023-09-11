ebf3e28ae992114f6b98e9a8ed21f0be9def854c
alicesouza.devrise <alicesouza.devrise@gmail.com>
2023-09-11 09:44:02 -0300

    Finish fetch and filter opstions

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,32 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import { Api } from './api/shared/api/axios-config';
+import { Card } from './components/card/Card';
+import { InputSelect } from './components/inputs/input-select/InputSelect';
+import { InputText } from './components/inputs/input-text/InputText';
 
 function App() {
-  const teste = async (id: number) => {
+  const [countries, setCountries] = useState([])
+  const [search, setSearch] = useState("")
+  const [select, setSelect] = useState("")
+
+  const fetchAllCountries = async () => {
     const res = await Api.get(`/country/`)
-    console.log(res.data)
+    setCountries(res.data)
   }
 
-  teste(1)
+  const filterCountries = () => {
+    var newCountries = [...countries]
+    console.log(newCountries)
+    if (search) {
+      newCountries = newCountries.filter((c: any) => {
+        return c.id.toString().includes(search) ||
+        c.name.toLocaleLowerCase().includes(search.toLocaleLowerCase()) ||
+        c.capital.toLocaleLowerCase().includes(search.toLocaleLowerCase()) ||
+        c.continent.toLocaleLowerCase().includes(search.toLocaleLowerCase())
+      })
+    }
+
+    if (select) {
+      newCountries = newCountries.filter((c: any) => {
+        return c.continent.toLocaleLowerCase().includes(select.toLocaleLowerCase())
+      })
+    }
+    return newCountries
+  }
+
+  const continents = [
+    {id: "Ásia", name: "Ásia"},
+    {id: "África", name: "África"},
+    {id: "América", name: "América"},
+    {id: "Europa", name: "Europa"},
+    {id: "Oceania", name: "Oceania"},
+  ]
+
+  useEffect(() => {
+    if (countries.length <= 0) {
+      fetchAllCountries()
+    }
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <nav>
+      <InputText
+        placeholder="Pesquisar..."
+        onChange={(e: any) => setSearch(e.target.value)}
+      />
+      <InputSelect
+        options={continents}
+        onChange={(e: any) => setSelect(e.target.value)}
+      />
+
+    </nav>
+    {filterCountries().map((c: any) => {
+      return (
+        <Card
+          country={c.name}
+          capital={c.capital}
+          continent={c.continent}
+        />
+      )
+    })}
+    </>
   );
 }
 
